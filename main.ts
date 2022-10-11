57a91ab7a304dc15dea242cea650f270b6ecb4fb
Alexey Kalmakov (rndD) <st00nsa@gmail.com>
2022-10-12 01:35:07 +0200

    Initial commit, it works but need to be polished

--- main.ts
+++ main.ts
@@ -1,137 +1,214 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+  App,
+  Editor,
+  editorEditorField,
+  FileSystemAdapter,
+  MarkdownView,
+  Modal,
+  Notice,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+} from 'obsidian'
+import { uploadToPaper } from 'src/dropbox'
+import * as path from 'path'
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface Settings {
+  apiKey: string
+  prefixPath: string
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: Settings = {
+  apiKey: 'default',
+  prefixPath: '/paper/',
 }
-
+const DEFAULT_TOOLTIP_TEXT = 'Publish to Paper'
+// const DEFAULT_ICON = 'cloud-upload';
+const API_KEY_HELP_URL =
+  'https://help.displayr.com/hc/en-us/articles/360004116315-How-to-Create-an-Access-Token-for-Dropbox'
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  settings: Settings
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings()
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    // // This creates an icon in the left ribbon.
+    // const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+    // 	// Called when the user clicks the icon.
+    // 	new Notice('This is a notice!');
+    // });
+    // // Perform additional things with the ribbon
+    // ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+    const statusBarItemEl = this.addStatusBarItem()
+    statusBarItemEl.setText(DEFAULT_TOOLTIP_TEXT)
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    // This adds a simple command that can be triggered anywhere
+    // this.addCommand({
+    // 	id: 'open-sample-modal-simple',
+    // 	name: 'Open sample modal ()',
+    // 	callback: () => {
+    // 		new SampleModal(this.app).open();
+    // 	}
+    // });
+    // This adds an editor command that can perform some operation on the current editor instance
+    const publish = async () => {
+      const { apiKey, prefixPath } = this.settings
+      if (!apiKey || !prefixPath) {
+        new Notice('Please set your API key and prefix path in the settings.')
+        return
+      }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+      const relativePath = this.app.workspace.getActiveFile().path
+      let absolutePath = ''
+      const adapter = app.vault.adapter
+      if (adapter instanceof FileSystemAdapter) {
+        absolutePath = path.join(adapter.getBasePath(), relativePath)
+      } else {
+        // throw error about adapter not being a FileSystemAdapter
+        new Notice('This plugin only works with local files.')
+        return
+      }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+      statusBarItemEl.setText('Publishing...')
+      try {
+        const url = await uploadToPaper(absolutePath, apiKey, prefixPath)
+        statusBarItemEl.setText('Published')
+        navigator.clipboard.writeText(url).then(() => {
+          new Notice(`Published to Paper: ${url} copied to clipboard.`)
+        })
+        this.registerInterval(
+          window.setInterval(() => {
+            statusBarItemEl.setText(DEFAULT_TOOLTIP_TEXT)
+          }, 3 * 1000),
+        )
+      } catch (e) {
+        new Notice(`Error: ${e}`)
+        statusBarItemEl.setText(DEFAULT_TOOLTIP_TEXT)
+      }
+      // console.log(editor.getSelection());
+      // editor.replaceSelection('Sample Editor Command');
+    }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    statusBarItemEl.onClickEvent(() => {
+      publish()
+    })
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    this.addCommand({
+      id: 'publish-to-paper',
+      name: 'Publish to Dropbox Paper',
+      callback: publish,
+    })
+    // This adds a complex command that can check whether the current state of the app allows execution of the command
+    // this.addCommand({
+    // 	id: 'open-sample-modal-complex',
+    // 	name: 'Open sample modal (complex)',
+    // 	checkCallback: (checking: boolean) => {
+    // 		// Conditions to check
+    // 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+    // 		if (markdownView) {
+    // 			// If checking is true, we're simply "checking" if the command can be run.
+    // 			// If checking is false, then we want to actually perform the operation.
+    // 			if (!checking) {
+    // 				new SampleModal(this.app).open();
+    // 			}
 
-	onunload() {
+    // 			// This command will only show up in Command Palette when the check function returns true
+    // 			return true;
+    // 		}
+    // 	}
+    // });
 
-	}
+    this.addSettingTab(new SettingTab(this.app, this))
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+    // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+    // Using this function will automatically remove the event listener when this plugin is disabled.
+    // this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+    // 	console.log('click', evt);
+    // });
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+    // this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+  }
+
+  onunload() {}
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings)
+  }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+// class SampleModal extends Modal {
+// 	constructor(app: App) {
+// 		super(app);
+// 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+// 	onOpen() {
+// 		const {contentEl} = this;
+// 		contentEl.setText('Woah!');
+// 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+// 	onClose() {
+// 		const {contentEl} = this;
+// 		contentEl.empty();
+// 	}
+// }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class SettingTab extends PluginSettingTab {
+  plugin: MyPlugin
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: MyPlugin) {
+    super(app, plugin)
+    this.plugin = plugin
+  }
 
-	display(): void {
-		const {containerEl} = this;
+  display(): void {
+    const { containerEl } = this
 
-		containerEl.empty();
+    containerEl.empty()
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+    containerEl.createEl('h2', {
+      text: 'Obsidian note publish to Dropbox Paper',
+    })
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    containerEl.createEl('a', {
+      text: 'How to get Dropbox Paper API key',
+      href: API_KEY_HELP_URL,
+    })
+
+    new Setting(containerEl)
+      .setName('API Key')
+      .setDesc('Enter your API key for Dropbox Paper')
+      .addText((text) =>
+        text
+          .setPlaceholder('Enter your secret key here')
+          .setValue(this.plugin.settings.apiKey)
+          .onChange(async (value) => {
+            this.plugin.settings.apiKey = value
+            await this.plugin.saveSettings()
+          }),
+      )
+
+    new Setting(containerEl)
+      .setName('Paper directory path')
+      .setDesc(
+        'Enter the path to the directory where you want to save your published notes',
+      )
+      .addText((text) =>
+        text
+          .setPlaceholder('/paper/')
+          .setValue(this.plugin.settings.prefixPath)
+          .onChange(async (value) => {
+            this.plugin.settings.prefixPath = value
+            await this.plugin.saveSettings()
+          }),
+      )
+  }
 }
---
+++ src/dropbox.ts
@@ -0,0 +1,64 @@
+import { readFile } from 'fs/promises'
+import * as path from 'path'
+
+const API_URLS = {
+  paper_create: 'https://api.dropboxapi.com/2/files/paper/create',
+  share_link:
+    'https://api.dropboxapi.com/2/sharing/create_shared_link_with_settings',
+}
+
+export const uploadToPaper = async (
+  filepath: string,
+  apiKey: string,
+  paperPrefix: string,
+) => {
+  const basename = path.basename(filepath)
+  const file = await readFile(filepath)
+  const paperPath = path.join('/', paperPrefix, basename + '.paper')
+
+  const response = await fetch(API_URLS.paper_create, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+      'Content-Type': 'application/octet-stream',
+      'Dropbox-API-Arg': JSON.stringify({
+        path: paperPath,
+        import_format: 'markdown',
+      }),
+    },
+    body: file,
+  })
+  // TODO read body and check for errors in status
+  const json = await response.json()
+
+  if (json.error) {
+    throw new Error(json.error_summary)
+  }
+  return json.url
+  //   const resultPath = json.result_path
+
+  //   const response2 = await fetch(API_URLS.share_link, {
+  //     method: 'POST',
+  //     headers: {
+  //       Authorization: `Bearer ${apiKey}`,
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({
+  //       path: resultPath,
+  //       settings: {
+  //         access: 'viewer',
+  //         allow_download: true,
+  //         audience: 'public',
+  //         requested_visibility: 'public',
+  //       },
+  //     }),
+  //   })
+  //   // TODO read body and check for errors in status
+  //   const json2 = await response2.json()
+
+  //   if (json2.error) {
+  //     throw new Error(json.error_summary)
+  //   }
+
+  //   return json2.url
+}
